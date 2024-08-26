52e2a01734fc3235c614222d69c7626dc4d1b149
kathcerttt <118830720+kathcerttt@users.noreply.github.com>
2024-08-27 03:09:16 +0800

    Update HangmanWordGenerator Class.py

--- HangmanWordGenerator Class.py
+++ HangmanWordGenerator Class.py
@@ -26,7 +26,6 @@ class HangmanWordGenerator:
       if category in self.categories: 
         word = random.choice(self.categories[category])
         return word
-''' if the category exists, it gets a random word from the categories and the word  gets returned'''
+
       else:
-  ''' if category is not in the list'''
         raise ValueError ("Category not found")
