e190af7b9884e916ebd00ae653693399c2b7f899
kathcerttt <118830720+kathcerttt@users.noreply.github.com>
2024-08-27 03:07:11 +0800

    Update HangmanWordGenerator Class.py

--- HangmanWordGenerator Class.py
+++ HangmanWordGenerator Class.py
@@ -1,1 +1,32 @@
+import random 
 
+class HangmanWordGenerator:
+  def __init__ (self):
+    self.categories = {
+          'Music Genres': [ 'Rock', 'Jazz', 'Pop', 'Classical', 'Blues',
+                        'Alternative Rock', 'Metal', 'Funk', 'Country', 'Hip Hop'],
+
+          'Cities' : ['Paris', 'London', 'New York', 'Tokyo', 'Seoul', 'Dubai',
+                      'Manila', 'Jakarta', 'Kuala Lumpur', 'Rome'],
+           
+        
+          'Animals' : ['Giraffe', 'Duck', 'Penguin', 'Parrot', 'Monkey',
+                       'Turtle', 'Jaguar', 'Mantis', 'Shark', 'Spider'],
+
+          'School Supplies': ['Paper', 'Highlighter', 'Marker', 'Eraser', 'Glue'
+                              'Crayons', 'Watercolor', 'Pencil', 'Ballpen', 'Notebook'],  
+
+          'Food': ['Yogurt', 'Tomato', 'Bagel', 'Chocolate', 'Potato', 'Croissant',
+                   'Asparagus', 'Barbecue', 'Dragonfruit', 'Jalapeno']   
+  
+            }
+    
+
+    def get_secret_word (self, category): 
+      if category in self.categories: 
+        word = random.choice(self.categories[category])
+        return word
+''' if the category exists, it gets a random word from the categories and the word  gets returned'''
+      else:
+  ''' if category is not in the list'''
+        raise ValueError ("Category not found")
